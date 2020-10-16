e1c17fc82ad1ace5d242617d01b0e7c0839d78fc
Aiperi <aip.amankulova@gmail.com>
2020-10-17 00:49:22 +0600

    Adding layout for the main screen

--- app/src/main/java/com/example/exam/MainActivity.kt
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
+
 }
